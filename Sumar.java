e7c172506d3ab6e05a2210135c7fb9118aaca7b7
MarcoRivera06 <marco.rivera@epn.edu.ec>
2026-04-07 08:25:53 -0500

    prj inicial

---
+++ Sumar.java
@@ -0,0 +1,9 @@
+public class Sumar {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int suma = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + suma);
+    }
+}
