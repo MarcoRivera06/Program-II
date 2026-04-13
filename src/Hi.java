836908ef7cd5882fbcfbc795f1d8ce06ec097ebb
MarcoRivera06 <marco.rivera@epn.edu.ec>
2026-04-13 18:40:37 -0500

    Fix: change nick

--- src/Hi.java
+++ src/Hi.java
@@ -1,6 +1,6 @@
 public class Hi {
     public static void main(String[] args) {
-        System.out.println("Hi, Celtic!");
+        System.out.println("Hi, Markin!");
         System.out.println("Hi, amo");
     }
 }
